451e29d11e14bfcf538cacf1a234f7c3f9fe302d
joshua.lo <joshua.lo@team.reachnow.com>
2019-08-16 10:08:31 -0700

    3

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const input = fs.createReadStream('dict.txt', {encoding: 'utf-8'});
 const lineReader = readline.createInterface({ input });
 
 let prefix = null
+let regex = null
 const options = []
 
 const results = []
@@ -14,12 +15,21 @@ for (let i = 2; i < process.argv.length; i++) {
         options.push(process.argv[i].substr(1))
     } else {
         prefix = process.argv[i]
+        if (prefix.indexOf('?') !== -1) {
+            regex = new RegExp(`^${prefix.replace('?', '.')}.*`)
+        }
     }
 }
 
 lineReader.on('line', (line) => {
-    if (line.indexOf(prefix) === 0) {
-        results.push(line)
+    if (regex) {
+        if (line.search(regex) === 0) {
+            results.push(line)
+        }
+    } else {
+        if (line.indexOf(prefix) === 0) {
+            results.push(line)
+        } 
     }
 });
 
